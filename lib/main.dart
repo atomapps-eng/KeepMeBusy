7972e093f37f6adc76378e9571886d03faa8bd1b
Basuki Rahmat <atom.apps02@gmail.com>
2026-01-27 14:32:54 +0700

    backup setting page & main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'theme/app_theme.dart';
-import 'auth_gate.dart';
+import 'auth_gate.dart';//
 
 
 
